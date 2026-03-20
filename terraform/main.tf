b0a1fbfd9a1dfaf83c9d237aaa517956134ffbd9
Chad Sandquist <chadsandquist@msn.com>
2026-03-20 10:16:39 -0400

    chore/skeleton

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.10.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    # Pre-create this bucket before running terraform init
+    bucket = "outreach-newsletter-terraform-state"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
